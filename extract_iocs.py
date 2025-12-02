fe84bbf1fb934a8b2dc3856d9b543a0394ce61c4
Aggio98 <coder@aggioworld.com>
2025-12-02 21:00:38 +0000

    finding the IOCs in file.txt with regex

--- extract_iocs.py
+++ extract_iocs.py
@@ -1,3 +1,14 @@
+import re
+
 with open("file.txt", "r") as file:
     data = file.read()
-    
+
+#Regex to find all emails, ips, and domains
+email_pattern = r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,63}\b'
+domain_pattern = r'\b(?=.{4,253}\b)(?:[a-zA-Z0-9-]{1,63}\.)+[A-Za-z]{2,63}\b'
+ip_pattern = r'\b(?:(?:25[0-5]|2[0-4]\d|1?\d?\d)(?:\.|$)){4}\b'
+
+#Using re package to find them in the file 
+emails = re.findall(email_pattern, data)
+domains = re.findall(domain_pattern, data)
+ips = re.findall(ip_pattern, data)
