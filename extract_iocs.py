1265149e8e9a795aa6e0acef831b2707176bfd59
Aggio98 <coder@aggioworld.com>
2025-12-02 21:05:23 +0000

    had to fix the ip regex and also added so that there cannot be duplicates, saved the ips,domains and emails into the json file

--- extract_iocs.py
+++ extract_iocs.py
@@ -1,5 +1,6 @@
 import re
 import ipaddress
+import json
 
 with open("file.txt", "r") as file:
     data = file.read()
@@ -7,7 +8,7 @@ with open("file.txt", "r") as file:
 #Regex to find all emails, ips, and domains
 email_pattern = r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,63}\b'
 domain_pattern = r'\b(?=.{4,253}\b)(?:[a-zA-Z0-9-]{1,63}\.)+[A-Za-z]{2,63}\b'
-ip_pattern = r'\b(?:(?:25[0-5]|2[0-4]\d|1?\d?\d)(?:\.|$)){4}\b'
+ip_pattern = r'[0-9]+(?:\.[0-9]+){3}'
 
 #Using re package to find them in the file 
 emails = re.findall(email_pattern, data)
@@ -22,3 +23,16 @@ for ip in ips:
         valid_ips.append(ip)
     except ipaddress.AddressValueError:
         pass
+
+emails = list(set(emails))
+domains = list(set(domains))
+valid_ips = list(set(valid_ips))
+
+json_data = {
+    "ips": valid_ips,
+    "domains": domains,
+    "emails": emails
+}
+
+with open("info.json", "w") as outfile:
+    json.dump(json_data, outfile, indent=4)
